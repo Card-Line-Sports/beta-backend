c24512685596542bedae046eb49d9fcc3fab0fb5
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2022-07-23 21:37:17 +0400

    minor changes

--- packages/beta-backend-code/src/lib/League.ts
+++ packages/beta-backend-code/src/lib/League.ts
@@ -3,59 +3,60 @@
 import { gameState } from './Game';
 
 export class League {
-  currWeek: number;
-  cumulativeRankingsSum?: Map<number, number>; //map from player ID to their total rankings sum from each week
-  currRankings?: Map<number, number>; //map from player ID to their current rank (as calculated from cumulativeRankings)
-  gameStates: Map<number, gameState>; //map from week number to gameState object for that week
+	currWeek: number;
+	cumulativeRankingsSum?: Map<number, number>; //map from player ID to their total rankings sum from each week
+	currRankings?: Map<number, number>; //map from player ID to their current rank (as calculated from cumulativeRankings)
+	gameStates: Map<number, gameState>; //map from week number to gameState object for that week
 
-  //game state initialization
-  //weekly budget, array of players
+	//game state initialization
+	//weekly budget, array of players
 
-  constructor(totalWeeks: number) {
-    this.currWeek = 1;
+	constructor(totalWeeks: number) {
+		this.currWeek = 1;
 
-    this.gameStates = new Map<number, gameState>();
-    for (let i = 1; i <= totalWeeks; i++) {
-      this.gameStates.set(i, new gameState()); // pass into gamestate some array of players IDs, weekly budget
-    }
-  }
+		this.gameStates = new Map<number, gameState>();
+		for (let i = 1; i <= totalWeeks; i++) {
+			this.gameStates.set(i, new gameState()); // pass into gamestate some array of players IDs, weekly budget
+		}
+	}
 
-  /*
+	/* Start current week's game, called once a week by our internal server. Returns the gameState object upon which the user can
+  make function calls
+  */
+	startCurrWeek(): gameState {
+		const currGameState = this.gameStates.get(this.currWeek);
+		currGameState.setLineData();
+		return currGameState;
+	}
+
+	/* Ends the current week's game. Called by internal server at the end of a week. Calls final result compilation functions.
+	 */
+	endCurrWeek() :  {
+		//get bet results
+		//update player budgets
+		//update final rankings of week
+		//update cumulative rankings in league class
+		this.currWeek++;
+
+    //if last week, then call 
+	}
+
+	/*
   Getter functions for viewing
   */
 
-  getPlayerData(playerID: number): any[] {
-    return [];
-  }
+	getPlayerData(playerID: number): any[] {
+		return [];
+	}
 
-  getRankings(): Map<number, number> {
-    return this.currRankings;
-  }
+	getRankings(): Map<number, number> {
+		return this.currRankings;
+	}
 
-  /*
+	/*
   Following functions are only callable by the league admin
   */
-  //Functions regarding adding, removing, editing players:
-  // addPlayer(name: string) {
-  //   let id = this.players.size;
-  //   let newPlayer = new Player(id, name);
-  //   this.players.set(id, newPlayer);
-  //   return newPlayer;
-  // }
-
-  // removePlayer(id: number): void {
-  //   let removed = this.players[id];
-  //   this.players.delete(id);
-  //   return removed;
-  // }
-
-  // editPlayer(id: number, name: string) {
-  //   let playerOriginal = this.players.get(id);
-  //   playerOriginal.setName(name);
-  //   this.players.set(id, playerOriginal);
-  //   /*returns updated player */
-  //   return playerOriginal;
-  // }
+	registerPlayer(name: string) {}
 }
 
 export default League;
